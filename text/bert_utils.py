8452016572e3858a09b6819e0d02c23db9317005
Huy <93850157+HuuHuy227@users.noreply.github.com>
2024-02-29 12:26:25 +0700

    Update bert_utils.py

--- text/bert_utils.py
+++ text/bert_utils.py
@@ -19,5 +19,5 @@ def _check_bert(repo_id, files, local_path):
             #     )
             # else:
             hf_hub_download(
-                    repo_id, file, local_dir=local_path, local_dir_use_symlinks=False
+                    "vinai/" + repo_id, file, local_dir=local_path, local_dir_use_symlinks=False
                 )
