710f7fd3f95e6d4005879be827fca23ec95d3636
Huy <93850157+HuuHuy227@users.noreply.github.com>
2024-02-29 10:40:52 +0700

    Update vietnamese.py

--- text/vietnamese.py
+++ text/vietnamese.py
@@ -221,15 +221,15 @@ def g2p(text):
     word2ph = []
 
     text = text.replace('\s+',' ').lower()
-    words = vi2IPA_split(text,delimit="/").split()
+    words, text_normalize = vi2IPA_split(text,delimit="/").split()
 
-    word_seg = segment_sentence(text)
+    word_seg = segment_sentence(text_normalize)
     input_ids = tokenizer.encode(word_seg)
     toks = [tokenizer._convert_id_to_token(ids) for ids in input_ids[1:-1]]
 
     if len(toks) != len(words): #Handle conflict between phoTokenizer and word segments
         words = refine_tok(words, toks)
-    print(words)
+    #print(words)
     for word in words:
         if "_" in word: # handle TH tu ghep vd: vi_tri nghien_cuu_vien, ...
             ph_count = 0
